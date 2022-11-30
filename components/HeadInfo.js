b31c54d1c5d5ebfb8ef20544326399a3376df49e
Donovan Gomez <donovanjuniogomez@gmail.com>
2022-11-30 01:59:54 -0800

    add meta content to Head component

--- components/HeadInfo.js
+++ components/HeadInfo.js
@@ -5,7 +5,12 @@ export default function HeadInfo({ title }) {
   return (
     <Head>
       <title>FGC Resources | {title}</title>
+      <meta  name="description" content={`Resource hub of Fighting game resources for ${title}`} />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta
+          property="og:image"
+          content="/assets/footsies.gif"
+        />
     </Head>
   );
 }
